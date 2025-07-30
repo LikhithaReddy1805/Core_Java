964022f9787eab7b68f88961fe0399ef0a58cea9
likhi <likhi@LAPTOP-6762QIHG>
2025-07-30 13:57:14 +0530

    Commit

---
+++ Core_Java_project/src/UserName.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+/**
+ * 
+ */
+public class UserName {
+
+	public static void main(String[] args) {
+		
+		Scanner input=new Scanner(System.in);
+		System.out.println("Enter name:");
+		String n=input.nextLine();
+		System.out.println("Hello"+n);
+		
+
+	}
+
+}
