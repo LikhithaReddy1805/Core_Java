24df607c5db76252f8998c4d83d9ded45cd72078
likhi <likhi@LAPTOP-6762QIHG>
2025-08-17 22:38:30 +0530

    commit

--- Core_Java_project/src/com/tnsif/usingfinal/FinalMethodDemo.java
+++ Core_Java_project/src/com/tnsif/usingfinal/FinalMethodDemo.java
@@ -1,19 +1,15 @@
 package com.tnsif.usingfinal;
 
-public class FinalMethod {
-	
-	final int a=10;
-	
-	public FinalMethod() {
-		System.out.println("In the default constructor");
-	}
-	
-	final void show() {
-		System.out.println("The value of a "+a);
-	}
-	
-	void display(){
+public class FinalMethodDemo extends FinalMethod {
+	//Can't overided final method from super class
+	//void show(){
 		
+	//}
+
+	@Override
+	void display() {
+
 	}
 
+	
 }
